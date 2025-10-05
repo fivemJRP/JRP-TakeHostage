72e6e9f0343810beffd60b66f0d55b70729e79ef
Jer <69177114+ImJer@users.noreply.github.com>
2025-10-05 03:09:06 -0600

    Refactor indentation and fix permission check
    Replaced tabs with spaces for consistent indentation and updated permission check to use IsPlayerAceAllowed instead of IsAceAllowed in sv_takehostage.lua.

--- sv_takehostage.lua
+++ sv_takehostage.lua
@@ -5,64 +5,64 @@ local takenHostage = {}
 
 RegisterServerEvent("TakeHostage:sync")
 AddEventHandler("TakeHostage:sync", function(targetSrc)
-	local source = source
-	TriggerClientEvent("TakeHostage:syncTarget", targetSrc, source)
-	takingHostage[source] = targetSrc
-	takenHostage[targetSrc] = source
+    local source = source
+    TriggerClientEvent("TakeHostage:syncTarget", targetSrc, source)
+    takingHostage[source] = targetSrc
+    takenHostage[targetSrc] = source
 end)
 
 RegisterServerEvent("TakeHostage:releaseHostage")
 AddEventHandler("TakeHostage:releaseHostage", function(targetSrc)
-	local source = source
-	if takenHostage[targetSrc] then
-		TriggerClientEvent("TakeHostage:releaseHostage", targetSrc, source)
-		takingHostage[source] = nil
-		takenHostage[targetSrc] = nil
-	end
+    local source = source
+    if takenHostage[targetSrc] then
+        TriggerClientEvent("TakeHostage:releaseHostage", targetSrc, source)
+        takingHostage[source] = nil
+        takenHostage[targetSrc] = nil
+    end
 end)
 
 RegisterServerEvent("TakeHostage:killHostage")
 AddEventHandler("TakeHostage:killHostage", function(targetSrc)
-	local source = source
-	if takenHostage[targetSrc] then
-		TriggerClientEvent("TakeHostage:killHostage", targetSrc, source)
-		takingHostage[source] = nil
-		takenHostage[targetSrc] = nil
-	end
+    local source = source
+    if takenHostage[targetSrc] then
+        TriggerClientEvent("TakeHostage:killHostage", targetSrc, source)
+        takingHostage[source] = nil
+        takenHostage[targetSrc] = nil
+    end
 end)
 
 RegisterServerEvent("TakeHostage:stop")
 AddEventHandler("TakeHostage:stop", function(targetSrc)
-	local source = source
-	if takingHostage[source] then
-		TriggerClientEvent("TakeHostage:cl_stop", targetSrc)
-		takingHostage[source] = nil
-		takenHostage[targetSrc] = nil
-	elseif takenHostage[source] then
-		TriggerClientEvent("TakeHostage:cl_stop", targetSrc)
-		takenHostage[source] = nil
-		takingHostage[targetSrc] = nil
-	end
+    local source = source
+    if takingHostage[source] then
+        TriggerClientEvent("TakeHostage:cl_stop", targetSrc)
+        takingHostage[source] = nil
+        takenHostage[targetSrc] = nil
+    elseif takenHostage[source] then
+        TriggerClientEvent("TakeHostage:cl_stop", targetSrc)
+        takenHostage[source] = nil
+        takingHostage[targetSrc] = nil
+    end
 end)
 
 AddEventHandler('playerDropped', function(reason)
-	local source = source
-	if takingHostage[source] then
-		TriggerClientEvent("TakeHostage:cl_stop", takingHostage[source])
-		takenHostage[takingHostage[source]] = nil
-		takingHostage[source] = nil
-	end
-	if takenHostage[source] then
-		TriggerClientEvent("TakeHostage:cl_stop", takenHostage[source])
-		takingHostage[takenHostage[source]] = nil
-		takenHostage[source] = nil
-	end
+    local source = source
+    if takingHostage[source] then
+        TriggerClientEvent("TakeHostage:cl_stop", takingHostage[source])
+        takenHostage[takingHostage[source]] = nil
+        takingHostage[source] = nil
+    end
+    if takenHostage[source] then
+        TriggerClientEvent("TakeHostage:cl_stop", takenHostage[source])
+        takingHostage[takenHostage[source]] = nil
+        takenHostage[source] = nil
+    end
 end)
 
 RegisterServerEvent("TakeHostage:checkPermission")
 AddEventHandler("TakeHostage:checkPermission", function()
 	local source = source
-	if IsAceAllowed(source, "jrp.takehostage") then
+	if IsPlayerAceAllowed(source, "jrp.takehostage") then
 		TriggerClientEvent("TakeHostage:proceed", source)
 	else
 		TriggerClientEvent("TakeHostage:notify", source, "~r~JRP Platinum VIP Required! ~w~Buy it from the ~g~Server Store~w~!")
